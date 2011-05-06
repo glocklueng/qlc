65637c7790650e3bf89c4cb0d3ff827c77ee6139
hjunnila <hjunnila@0db28fee-5c31-0410-96ea-e69cce70e049>
2011-05-06 19:41:55 +0000

    Added UI tests to Windows unittest.bat.
    git-svn-id: https://qlc.svn.sourceforge.net/svnroot/qlc/trunk@2360 0db28fee-5c31-0410-96ea-e69cce70e049

--- unittest.bat
+++ unittest.bat
@@ -1,17 +1,24 @@
 @ECHO OFF
 
 pushd .
-
 REM Enttec wing test
+cd plugins\ewinginput\src
+..\test\test_ewing.exe
+IF NOT %ERRORLEVEL%==0 exit /B %ERRORLEVEL%
+popd
+
+pushd .
+REM Velleman test
+cd plugins\vellemanout\src
 SET OLDPATH=%PATH%
-PATH=%PATH%;plugins\ewinginput\src
-plugins\ewinginput\test\test_ewing.exe
+PATH=%PATH%;C:\k8062d
+REM Surprise, surprise, windows doesn't know how to handle mock objects *sigh*
+REM ..\test\vellemanout_test.exe
 IF NOT %ERRORLEVEL%==0 exit /B %ERRORLEVEL%
 SET PATH=%OLDPATH%
-
 popd
-pushd .
 
+pushd .
 REM Engine test
 cd engine\test
 SET OLDPATH=%PATH%
@@ -19,19 +26,48 @@ PATH=%PATH%;..\src
 test_engine.exe
 IF NOT %ERRORLEVEL%==0 exit /B %ERRORLEVEL%
 SET PATH=%OLDPATH%
-
 popd
+
 pushd .
-
 REM UI test
-
 cd ui\test
 SET OLDPATH=%PATH%
-PATH=%PATH%;..\..\engine\src
-aboutbox\aboutbox_test.exe
-IF NOT %ERRORLEVEL%==0 exit /B %ERRORLEVEL%
-addfixture\addfixture_test.exe
-IF NOT %ERRORLEVEL%==0 exit /B %ERRORLEVEL%
-SET PATH=%OLDPATH%
+PATH=%PATH%;..\..\..\engine\src;..\..\..\ui\src
 
+cd aboutbox
+aboutbox_test.exe
+IF NOT %ERRORLEVEL%==0 exit /B %ERRORLEVEL%
+cd ..
+
+cd addfixture
+addfixture_test.exe
+IF NOT %ERRORLEVEL%==0 exit /B %ERRORLEVEL%
+cd ..
+
+cd vcframe
+vcframe_test.exe
+IF NOT %ERRORLEVEL%==0 exit /B %ERRORLEVEL%
+cd ..
+
+cd vclabel
+vclabel_test.exe
+IF NOT %ERRORLEVEL%==0 exit /B %ERRORLEVEL%
+cd ..
+
+cd vcproperties
+vcproperties_test.exe
+IF NOT %ERRORLEVEL%==0 exit /B %ERRORLEVEL%
+cd ..
+
+cd vcwidgetproperties
+vcwidgetproperties_test.exe
+IF NOT %ERRORLEVEL%==0 exit /B %ERRORLEVEL%
+cd ..
+
+cd vcxypadfixture
+vcxypadfixture_test.exe
+IF NOT %ERRORLEVEL%==0 exit /B %ERRORLEVEL%
+cd ..
+
+SET PATH=%OLDPATH%
 popd
