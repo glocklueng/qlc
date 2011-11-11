364c2e15e6d95d3717b55f334166200e15d22ec2
hjunnila <hjunnila@0db28fee-5c31-0410-96ea-e69cce70e049>
2011-11-11 15:32:49 +0000

    Added comments and licence information to RGB scripts
    git-svn-id: https://qlc.svn.sourceforge.net/svnroot/qlc/trunk@2664 0db28fee-5c31-0410-96ea-e69cce70e049

--- rgbscripts/fullrows.js
+++ rgbscripts/fullrows.js
@@ -1,4 +1,29 @@
+/*
+  Q Light Controller
+  fullrows.js
+
+  Copyright (c) Heikki Junnila
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  Version 2 as published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details. The license is
+  in the file "COPYING".
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
 (
+    /**
+     * This algorithm produces fully-lit rows, meaning all pixels on a single
+     * row are lit together.
+     */
     function()
     {
         var algo = new Object;
@@ -6,6 +31,14 @@
         algo.name = "Full Rows";
         algo.author = "Heikki Junnila";
 
+        /**
+         * The actual "algorithm" for this RGB script. Produces a map of
+         * size($width, $height) each time it is called.
+         *
+         * @param step The step number that is requested (0 to (algo.rgbMapStepCount - 1))
+         * @param rgb Tells the color requested by user in the UI.
+         * @return A two-dimensional array[height][width].
+         */
         algo.rgbMap = function(width, height, rgb, step)
         {
             var map = new Array(height);
@@ -24,8 +57,17 @@
             return map;
         }
 
+        /**
+         * Tells RGB Matrix how many steps this algorithm produces with size($width, $height)
+         *
+         * @param width The width of the map
+         * @param height The height of the map
+         * @return Number of steps required for a map of size($width, $height)
+         */
         algo.rgbMapStepCount = function(width, height)
         {
+            // Each row is lit completely at a time, so because there are $height rows
+            // in the map, the number of steps must be $height to light all rows per round.
             return height;
         }
 
